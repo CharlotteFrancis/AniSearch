e4b07aae58bcf99242bc9fc5a9db6f247206918c
Jeckfeldt <jeckfeldt123@gmail.com>
2021-05-20 11:40:32 -0700

    added search by genre

--- app.js
+++ app.js
@@ -4,17 +4,33 @@
 
 document.getElementById('submitGenre').addEventListener('click', event => {
   event.preventDefault()
-  axios.get(`https://kitsu.io/api/edge/anime?filter[categories]=adventure`)
+  axios.get(`https://kitsu.io/api/edge/anime?filter[categories]=${document.getElementById('genre').value}`)
   .then(res => {
-    let results = res.data
-    if(results.length > 1) {
-      console.log('works')
+    //grab results array
+    let results = res.data.data
+    console.log(results)
+    if(results.length <= 0) {
+      console.log('No results found.')
     } else {
-      console.log('doesnt work')
+      //appends 10 list elements to the ul aniList
+      for(let i = 0; i < 10 || i < results.length; i++) {
+        //console.log(results[i])
+        let listElem = document.createElement('li')
+        listElem.innerHTML = `
+        <div class="card" style="width: 18rem;">
+          <img src="${results[i].attributes.posterImage.small}" class="card-img-top" alt="${results[i].attributes.titles.en_jp}">
+            <div class="card-body">
+              <h5 class="card-title">${results[i].attributes.titles.en_jp}</h5>
+              <p class="card-text">${results[i].attributes.description}</p>
+              <a id="favorite" class="btn btn-primary">Add to Favorites</a>
+            </div>
+        </div>
+        `
+        document.getElementById('aniList').append(listElem)
+      }
     }
   })
-
-
+  .catch(err => console.error(err))
 
 
 })
